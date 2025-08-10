f5eb7579192c549f195e649d9d7199119d81e0c5
volkamd <msnti@bk.ru>
2025-08-10 20:10:06 +0700

    Update decoration and cheks.cpp

--- snake 2025-07-27/decoration and cheks.cpp
+++ snake 2025-07-27/decoration and cheks.cpp
@@ -10,11 +10,8 @@ bool check(point head, wall wal) {
 		return 1;
 	}
 	return 0;
-
 }
-
 void food::spawn(wall wal) {
-	//coordinate = ;
 	srand(time(NULL));
 	if (coordinate.empty() == 1) {
 		this->coordinate.push_back(point(rand() % (wal.getXbord() - 1) + 1, rand() % (wal.getYbord() - 1) + 1));
@@ -30,25 +27,17 @@ void food::spawn(wall wal) {
 	SetConsoleCursorPosition(oop, cPoss);
 	printf("%c", 'A');
 }
-// прописать еду!!!
-
-
-
 bool food::check(point head) {
 	if (head.get_x() == coordinate[0].get_x() && head.get_y() == coordinate[0].get_y()) {
-		return 1;  // змея съела
+		return 1;
 	}
 	else return false;
 }
-// если checj дает 1 значит змея съела и надо заспавнить еду. если 0 то ничего. 
-
-
 int speed(int value) {
 	srand(time(0));
 	if (value == 55) {
 		return rand() % 900 + 100;
 	}
-	
 	if (value == 0) {
 		return 1300;
 	}
